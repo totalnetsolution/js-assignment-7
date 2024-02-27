62dcdf1f04fb345a221ccfbb1fa3f53a9aecfd6e
muhammad-danish <totalnetsolution2@gmail.com>
2024-02-28 01:54:42 +0500

    index file created

---
+++ app.js
@@ -0,0 +1,5 @@
+var visitor_name=("John Doe");
+var product_title=("T-shirt(s)");
+var quantity=("5");
+document.write("<b>" + visitor_name + "</b> ordered <b>" + quantity + " " + product_title + "</b> on XYZ Clothing store");                                
+                            
